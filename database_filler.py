b3ec543c3ff937cc2d90b34c5d35b094f100e1f2
Zayah117 <isaiah.baker@simpledove.com>
2017-08-30 11:57:29 -0400

    database_filler.py

---
+++ database_filler.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, User, Beer, Review
+
+engine = create_engine('sqlite:///data.db')
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession
+
+beer = Beer(name="Beer1", ibu=60, calories=120, abv=4.5, style="Style1", brewery_location="Location1")
+session.add(beer)
+
+beer = Beer(name="Beer2", ibu=60, calories=120, abv=4.5, style="Style2", brewery_location="Location2")
+session.add(beer)
+
+beer = Beer(name="Beer3", ibu=60, calories=120, abv=4.5, style="Style3", brewery_location="Location3")
+session.add(beer)
+
+beer = Beer(name="Beer4", ibu=60, calories=120, abv=4.5, style="Style4", brewery_location="Location4")
+session.add(beer)
+
+beer = Beer(name="Beer5", ibu=60, calories=120, abv=4.5, style="Style5", brewery_location="Location5")
+session.add(beer)
+
+session.commit()
