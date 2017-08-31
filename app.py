39a693c5f151af7d8b6606545aa9b40718e9c29e
Zayah117 <isaiah.baker@simpledove.com>
2017-08-31 10:16:18 -0400

    implement /beers put

--- app.py
+++ app.py
@@ -23,8 +23,37 @@ class HelloWorld(Resource):
 	def get(self):
 		return {'hello': 'world'}
 
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+
 class Beers(Resource):
 	def get(self):
+		beers = session.query(Beer).all()
+		return jsonify(Beers=[i.serialize for i in beers])
+
+	def put(self):
+		# put('http://localhost:5000/beers', data={'data': 'ANOTHER BEER'}).json()
+
+		'''
+		new_beer = Beer(name=request.form['name'],
+				ibu=request.form['ibu'],
+				calories=request.form['calories'],
+				abv=request.form['abv'],
+				style=request.form['style'],
+				brewery_location=request.form['brewery_location'])'''
+
+		new_beer = Beer(name=request.form['data'], ibu=0, calories=0, abv=0.0, style="style", brewery_location="location")
+		session.add(new_beer)
+		session.commit()
+
 		beers = session.query(Beer).all()
 		return jsonify(Beers=[i.serialize for i in beers])
 
@@ -36,6 +65,7 @@ class SingleBeer(Resource):
 api.add_resource(HelloWorld, '/')
 api.add_resource(Beers, '/beers')
 api.add_resource(SingleBeer, '/beer/<string:beer_id>')
+api.add_resource(TodoSimple, '/todo/<string:todo_id>')
 
 if __name__ == '__main__':
 	app.run(debug=True)
